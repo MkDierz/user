57f4d5cd645bdcaa8a0b52528ecb55ccba34f321
MkDierz <msyibbran24@gmail.com>
2023-07-13 08:02:36 +0700

    fix: :lock: implemet verify token to use get

--- utils/services.js
+++ utils/services.js
@@ -2,9 +2,7 @@ const { default: axios } = require('axios');
 const { baseUrl } = require('../config');
 
 const auth = {
-  verifyToken: (accessToken) => axios.post(`${baseUrl}/auth/verify-token`, {
-    accessToken,
-  }),
+  verifyToken: (accessToken) => axios.get(`${baseUrl}/auth/verify-token?accessToken=${accessToken}`),
   update: (data, token) => axios.put(`${baseUrl}/auth/update`, {
     ...data,
   }, {
